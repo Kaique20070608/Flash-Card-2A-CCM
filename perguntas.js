514197dedb5751cb1038e2f15ee70a06091e3c15
Kaique20070608 <164031461+Kaique20070608@users.noreply.github.com>
2024-10-29 12:06:43 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,9 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao(
+    'Educação física',
+    'O que é basquete',
+    'É aqueles jogo que os cara fica batendo a bola no chão'
+)
